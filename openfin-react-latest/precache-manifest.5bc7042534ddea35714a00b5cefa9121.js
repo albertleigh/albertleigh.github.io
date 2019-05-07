292cefdabef83184ced56ea230bd6eb38053c092
Wentao LI <liwentao90@yahoo.com>
2019-05-07 14:31:48 -0400

    0.30.1-beta

---
+++ openfin-react-latest/precache-manifest.5bc7042534ddea35714a00b5cefa9121.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3e0570fc31ef6e2e59ec4dbe7e62fc",
+    "url": "https://albertleigh.github.io/openfin-react-latest/index.html"
+  },
+  {
+    "revision": "b58a68e4db29b4b6397c",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/css/main.3bfbac93.chunk.css"
+  },
+  {
+    "revision": "264047ef904b0fe3bc47",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/js/2.dd314bb0.chunk.js"
+  },
+  {
+    "revision": "b58a68e4db29b4b6397c",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/js/main.52c8c034.chunk.js"
+  },
+  {
+    "revision": "b8199bd63efc9c594975",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/js/runtime~main.6da65a57.js"
+  },
+  {
+    "revision": "c45d4a8fe5a815b54640d3daa0160380",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/china.c45d4a8f.svg"
+  },
+  {
+    "revision": "b6836c40eb905871ee5cc94192c3e190",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/database-gears.b6836c40.svg"
+  },
+  {
+    "revision": "9576898a618a23c89e55547c4e9fef55",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/github-logo-dark.9576898a.svg"
+  },
+  {
+    "revision": "3ff8d8ca4f31c47036ba9bf6cd315eb8",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/github-logo.3ff8d8ca.svg"
+  },
+  {
+    "revision": "9a52440bf168eec46fdc050bfe0f80d6",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/logo.9a52440b.svg"
+  },
+  {
+    "revision": "863b9d022904994f52d061093021fbc9",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/material-ui-logo.863b9d02.svg"
+  },
+  {
+    "revision": "4ce0887be92625ea91ec439384be3f79",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/openfin.4ce0887b.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/react.5d5d9eef.svg"
+  },
+  {
+    "revision": "546aeb0eaac8bb21257d05c7b713a13a",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/settings.546aeb0e.svg"
+  },
+  {
+    "revision": "d37f770a5f40056134ca94453061cda5",
+    "url": "https://albertleigh.github.io/openfin-react-latest/static/media/united-states.d37f770a.svg"
+  }
+]);
